fccd6da0782fdebed55fd29428cc5c5155401fb2
Adam Rankin <adam.rankin@gmail.com>
2020-10-16 19:16:16 -0400

    Adding local var file for site specific settings

--- Common/Common_CMake.bat
+++ Common/Common_CMake.bat
@@ -1,3 +1,6 @@
 mkdir S4%1
 cd S4%1
-cmake -G "Visual Studio 16 2019" -A x64 -T v140 -DQt5_DIR:PATH=D:\Qt\5.12.0\msvc2017_64\lib\cmake\Qt5 -DCMAKE_CXX_MP_FLAG:BOOL=ON ..\S4
+
+call Local_Vars.bat
+
+cmake -G "Visual Studio 16 2019" -A x64 -T v140 -DQt5_DIR:PATH=%qt5_dir% -DCMAKE_CXX_MP_FLAG:BOOL=ON ..\S4
