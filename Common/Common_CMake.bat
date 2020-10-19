e5e647d20be339d68a5a069b7522175690b8276e
Adam Rankin <adam.rankin@gmail.com>
2020-10-19 14:10:21 -0400

    Path error

--- Common/Common_CMake.bat
+++ Common/Common_CMake.bat
@@ -1,6 +1,6 @@
 mkdir S4%1
 cd S4%1
 
-call Local_Vars.bat
+call ..\Common\Local_Vars.bat
 
 cmake -G "Visual Studio 16 2019" -A x64 -T v140 -DQt5_DIR:PATH=%qt5_dir% -DCMAKE_CXX_MP_FLAG:BOOL=ON ..\S4
