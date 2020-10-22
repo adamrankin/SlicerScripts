90423b82e1b02e9523818f226ca57ef3d0a472c7
Adam Rankin <adam.rankin@gmail.com>
2020-10-22 15:10:06 -0400

    Updating scripts for vs2019 and other new stuff

--- Common/Common_CopyDLLs.bat
+++ Common/Common_CopyDLLs.bat
@@ -1,13 +1,13 @@
 @echo off
 
-copy C:\d\Slcr\SCV%1\bin\%2\ITKVideoBridgeOpenCV-*.dll C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
-copy C:\d\Slcr\SCV%1\OpenCV-install\x64\vc14\bin\opencv_ffmpeg*.dll C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SCV%1\bin\%2\ITKVideoBridgeOpenCV-*.dll C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SCV%1\OpenCV-install\x64\vc16\bin\opencv_videoio_ffmpeg*.dll C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 
-copy C:\d\Slcr\SIGSIO%1\bin\%2\*.dll C:\d\Slcr\SIGSIO%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SIGSIO%1\bin\%2\*.dll C:\d\Slcr\SIGSIO%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 
-copy C:\d\Slcr\SOIGT%1\bin\%2\*.dll C:\d\Slcr\SOIGT%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SOIGT%1\bin\%2\*.dll C:\d\Slcr\SOIGT%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 
-copy C:\d\Slcr\SVR%1\OpenVR\bin\win64\openvr_api.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
-copy C:\d\Slcr\SVR%1\bin\%2\*.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SVR%1\OpenVR\bin\win64\openvr_api.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SVR%1\bin\%2\*.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 
-copy C:\d\Slcr\SCV%1\OpenCV-install\Lib\site-packages\cv2.cp36-win_amd64.pyd C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.11\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SCV%1\OpenCV-install\Lib\site-packages\cv2.cp36-win_amd64.pyd C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
