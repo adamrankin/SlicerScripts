321af1b88b80eebf863d9c0661b302409e8c6537
Adam Rankin <adam.rankin@gmail.com>
2021-09-09 08:51:21 -0400

    Missed change of extension name

--- Release_Designer.bat
+++ Release_Designer.bat
@@ -1,1 +1,1 @@
-.\S4R\Slicer-build\Slicer.exe --launcher-additional-settings C:/d/Slcr/SVRR/inner-build/AdditionalLauncherSettings.ini --launcher-additional-settings C:/d/Slcr/SVCR/AdditionalLauncherSettings.ini --designer
+.\S4R\Slicer-build\Slicer.exe --launcher-additional-settings C:/d/Slcr/SVRR/inner-build/AdditionalLauncherSettings.ini --launcher-additional-settings C:/d/Slcr/SPCR/AdditionalLauncherSettings.ini --designer
