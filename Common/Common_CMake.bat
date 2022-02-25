b8a987cd0c94170167cb1d74e07c495de016495a
Adam Rankin <adam.rankin@gmail.com>
2022-02-25 09:42:06 -0500

    VTK9 by default now

--- Common/Common_CMake.bat
+++ Common/Common_CMake.bat
@@ -3,4 +3,4 @@ cd S4%1
 
 call ..\Common\Local_Vars.bat
 
-cmake -G "Visual Studio 17 2022" -A x64 -DQt5_DIR:PATH=%qt5_dir% -DSlicer_VTK_VERSION_MAJOR:STRING=9 -DCMAKE_CXX_MP_FLAG:BOOL=ON ..\S4
+cmake -G "Visual Studio 17 2022" -A x64 -DQt5_DIR:PATH=%qt5_dir% -DCMAKE_CXX_MP_FLAG:BOOL=ON ..\S4
