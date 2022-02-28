4fecbd4aeac84f28cb9f53936d14ae123f11b72d
Adam Rankin <adam.rankin@gmail.com>
2022-02-28 16:14:56 -0500

    Updating for latest OpenCV version

--- Common/Common_CopyDLLs.bat
+++ Common/Common_CopyDLLs.bat
@@ -10,4 +10,4 @@ copy C:\d\Slcr\SOIGT%1\bin\%2\*.dll C:\d\Slcr\SOIGT%1\inner-build\lib\Slicer-4.1
 copy C:\d\Slcr\SVR%1\OpenVR\bin\win64\openvr_api.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 copy C:\d\Slcr\SVR%1\bin\%2\*.dll C:\d\Slcr\SVR%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
 
-copy C:\d\Slcr\SCV%1\OpenCV-install\Lib\site-packages\cv2.cp36-win_amd64.pyd C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
+copy C:\d\Slcr\SCV%1\OpenCV-install\Lib\site-packages\cv2.cp39-win_amd64.pyd C:\d\Slcr\SCV%1\inner-build\lib\Slicer-4.13\qt-loadable-modules\%2 /Y
